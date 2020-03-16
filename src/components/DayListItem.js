22b35b723bdb61bcbf3ebcdf290d6df4318d3204
Kevin <kevin.m.guertin@gmail.com>
2020-03-16 16:56:16 -0400

    add DayListItem style and passed props

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,16 @@
 import React from "react";
+import "components/DayListItem.scss";
+const classnames = require("classnames");
 
 export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
   return (
     <li
+      className={dayClass}
       onClick={() => {
         props.setDay(props.name);
       }}
