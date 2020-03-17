78a2a16f83d26c98fdb1515f46a9ac8f8a143460
Kevin <kevin.m.guertin@gmail.com>
2020-03-17 16:51:06 -0400

    add Confirm appointment component

---
+++ src/components/Appointment/Confirm.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "components/Button";
+
+export default function Confirm(props) {
+  return (
+    <main className="appointment__card appointment__card--confirm">
+      <h1 className="text--semi-bold">{props.message}</h1>
+      <section className="appointment__actions">
+        <Button danger onClick={props.onCancel}>
+          Cancel
+        </Button>
+        <Button danger onClick={props.onConfirm}>
+          Confirm
+        </Button>
+      </section>
+    </main>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -146,6 +147,15 @@ storiesOf("Appointment", module)
         interviewer={interviewer}
         onEdit={action("onEdit")}
         onDelete={action("onDelete")}
+      />
+    );
+  })
+  .add("Confirm", () => {
+    return (
+      <Confirm
+        message={"Delete the Appointment?"}
+        onConfirm={action("onConfirm")}
+        onCancel={action("onCancel")}
       />
     );
   });
