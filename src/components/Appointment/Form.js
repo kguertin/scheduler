310944387f6a0199b2bde175e32cb992106ab83a
Kevin <kevin.m.guertin@gmail.com>
2020-03-17 20:04:30 -0400

    add form components that use state

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,32 +1,45 @@
 import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
+import InterviewListItem from "components/InterviewerListItem";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
-            /*
-          This must be a controlled component
-        */
+            value={name}
+            onChange={e => {
+              setName(e.target.value);
+            }}
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          value={props.interviewer}
-          onChange={props.onChange}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
           <Button confirm onClick={props.onSave}>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -11,7 +11,7 @@ export default function InterviewerList(props) {
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.value}
-        setInterviewer={event => props.onChange(interviewer.id)}
+        setInterviewer={e => props.onChange(interviewer.id)}
       />
     );
   });
