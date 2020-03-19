86ec1737c6c17ca3081d9cd669958b9ef9685a60
Kevin <kevin.m.guertin@gmail.com>
2020-03-19 19:40:08 -0400

    add CREATE mode and onClick function for cancel

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 import useVisualMode from "../../hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
@@ -17,11 +18,26 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === EMPTY && (
+        <Empty
+          onAdd={() => {
+            transition(CREATE);
+          }}
+        />
+      )}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={[]}
+          onCancel={() => {
+            back();
+          }}
         />
       )}
     </article>
