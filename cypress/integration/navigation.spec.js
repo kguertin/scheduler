4f4921c027b6d2c06116ad72de9f1d8e2bf8c16a
Kevin <kevin.m.guertin@gmail.com>
2020-03-25 16:18:03 -0400

    add test to select day from side menu

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -8,8 +8,8 @@ describe("Navagation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/)");
 
-    cy.get("li")
-      .contains("Tuesday")
-      .click();
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
